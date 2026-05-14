3f0a888fce11765776690702e02f555a091da753
sistemas-snte56 <computo.seccional@outlook.com>
2026-05-13 23:21:00 -0600

    Modificacion titulo pagina

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consulta de Constancias - SNTE 56</title>
+    <title>Rifa 2026 - SNTE 56</title>
     
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
